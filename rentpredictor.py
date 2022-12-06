27c132dc8a3d47f67c3122e009880411170c2563
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-06 15:10:30 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -2,7 +2,6 @@ import streamlit as st
 st.set_page_config(page_icon=":house:", page_title="Rent Prediction")
 def check_password():
     def password_entered():
-        st.markdown("# Enter the password to access the Rent Predictor app.")
         if st.session_state["password"] == st.secrets["password"]:
             st.session_state["password_correct"] = True
             del st.session_state["password"]
@@ -10,7 +9,7 @@ def check_password():
             st.session_state["password_correct"] = False
     if "password_correct" not in st.session_state:
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+            " Enter the password to access the Rent Predictor app.", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
