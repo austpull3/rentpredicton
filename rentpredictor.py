ffca7e671ae7c314b71284cf0500ecf9b4cea639
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-07 10:49:47 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -1098,14 +1098,14 @@ if check_password():
             r.columns = ['Rent Type', 'Dogs', 'Smoking', 'Wheelchair', 'Electric Vehicle Charging', 'Furnished', 'Laundry', 'Parking', 'State', 'Bedroom/Bathroom', 'Predicted Rent Price']
             st.write(r)
             st.dataframe(r.head())
-            from FPDF import fpdf
+            from fpdf import FPDF
 
             st.header('PDF generator - test')
             button1 = st.button('PDF')
 
             if button1:
                 name = st.text_input('Name', value='')
-                pdf = fpdf('P', 'mm', 'A4')
+                pdf = FPDF('P', 'mm', 'A4')
                 pdf.add_page()
                 pdf.set_font(family='Times', size=16)
                 pdf.cell(40, 50, txt=name)
