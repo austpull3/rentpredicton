94125f5837aecfb6c23064a07a563685d56bc0a3
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-08 17:33:09 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -1128,7 +1128,21 @@ if check_password():
         plt.show()
 
         f.savefig("foo.pdf", bbox_inches='tight')
-        
+        export_as_pdf = st.button("Export Report")
+
+        def create_download_link(val, filename):
+            b64 = base64.b64encode(val)  # val looks like b'...'
+            return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
+        if export_as_pdf:
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_font('Arial', 'B', 16)
+            pdf.cell(40, 10, f)
+
+            html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+            st.markdown(html, unsafe_allow_html=True)
      
    
 
