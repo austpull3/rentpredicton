ce9e381eafbf768805486c04b2674a81e59431d1
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-08 19:48:24 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -141,7 +141,7 @@ if check_password():
 
         for col in df1.columns:
                 fig, ax = plt.subplots()
-                ax = sns.barplot(x = 'parking_options', y = 'price', data = df)
+                ax = sns.barplot()
                 st.pyplot(fig)
                 figs.append(fig)
 
