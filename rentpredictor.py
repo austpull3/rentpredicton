831da8fc1ed7e8077cb8c3b648b3d850881c32a1
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-08 17:03:31 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -1137,7 +1137,7 @@ if check_password():
         figs.append(h)
         pdf.add_page()
         with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
-            fig.savefig(tmpfile.name)
+            figs.savefig(tmpfile.name)
             pdf.image(tmpfile.name, 10, 10, 200, 100)
         html = create_download_link(pdf.output(dest="S").encode("latin-1"), "testfile")
         st.markdown(html, unsafe_allow_html=True)
