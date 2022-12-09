f5c80d43b87f336ce4f1bcc7a105f8c038970b70
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-08 17:55:07 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -134,27 +134,26 @@ if check_password():
         figs = []
         data = df.parking_options.value_counts()
         for i in range(1):
-            for col in df.columns:
-                fig, ax = plt.subplots()
-                ax.boxplot(data)
-                st.pyplot(fig)
-                figs.append(fig)
-            export_as_pdf = st.button("Export Report")
+            fig, ax = plt.subplots()
+                    ax.boxplot(data)
+                    st.pyplot(fig)
+                    figs.append(fig)
+                export_as_pdf = st.button("Export Report")
 
-            def create_download_link(val, filename):
-                b64 = base64.b64encode(val)  # val looks like b'...'
-                return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+                def create_download_link(val, filename):
+                    b64 = base64.b64encode(val)  # val looks like b'...'
+                    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-            if export_as_pdf:
-                pdf = FPDF()
-                pdf.add_page()
-                with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
-                    fig.savefig(tmpfile.name)
-                    pdf.image(tmpfile.name, 10, 10, 200, 100)
+                if export_as_pdf:
+                    pdf = FPDF()
+                    pdf.add_page()
+                    with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
+                        fig.savefig(tmpfile.name)
+                        pdf.image(tmpfile.name, 10, 10, 200, 100)
 
-                html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+                    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
 
-                st.markdown(html, unsafe_allow_html=True)
+                    st.markdown(html, unsafe_allow_html=True)
 
 
     def page2():
