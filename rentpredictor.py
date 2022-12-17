c6309fea64f66542a1ab876657ca10439fdf6f00
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-17 14:48:10 -0800

    Update rentpredictor.py

--- rentpredictor.py
+++ rentpredictor.py
@@ -265,6 +265,19 @@ if check_password():
         def create_download_link(val, filename):
             b64 = base64.b64encode(val)  # val looks like b'...'
             return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download Barplots</a>'
+        figs = []
+        d = df.head()
+        figs.append(d)
+        export_as_pdf = st.button("Export Report")
+        if export_as_pdf:
+            pdf = FPDF()
+            for fig in figs:
+                pdf.add_page()
+                with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
+                    fig.savefig(tmpfile.name)
+                    pdf.image(tmpfile.name, 10, 10, 200, 100)
+                    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "Rent Option Frequency Barplots")
+                    st.markdown(html, unsafe_allow_html=True)
 
 
         with tab2:
